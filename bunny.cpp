01a0c90e83f74c562b4d72a782cc20945cfa25e1
Karol <karol-tabaka@wp.pl>
2018-03-27 19:25:05 +0200

    Added methods bunny_count(), mutants_count(), list_bunnies(), live_bunnies(), add_bunny(), add_age() in bunny_herd class. To do: add kill_bunny(), breed_bunnies() and cull_bunies().

--- bunny.cpp
+++ bunny.cpp
@@ -2,23 +2,7 @@
 #include <iostream>
 #include "names.hpp"
 #include "bunny.hpp"
-/*
-#include "bunny.h"
-using namespace std;
-string boys_names[] = { "Bob", "Tom", "Rob"};
-int bunny::get_age()
-{
-	return age;
-}
-void bunny::set_age(int change_age)
-{
-	if(change_age <+ 0)
-	cout << "Wrong age change!" << endl;
-	else
-	age += change_age;
-}
 
-*/
 
 /**************************Class methods********************************/
 bunny::bunny()
